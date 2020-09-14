ad5b05165f1a6555955ce6f5c70646854171f193
ThiagoCMS <thicesar25@gmail.com>
2020-09-14 18:26:11 -0300

    Updates

---
+++ precache-manifest.f1718980989affd2480d10842bee55d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0503c0050161fe5c9f9d34f814d6a17e",
+    "url": "/github-profiles/index.html"
+  },
+  {
+    "revision": "df02f6be0bd273fa0f5d",
+    "url": "/github-profiles/static/css/main.6eeee02f.chunk.css"
+  },
+  {
+    "revision": "fd14ad4b630e14d72e1f",
+    "url": "/github-profiles/static/js/2.b25824f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-profiles/static/js/2.b25824f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df02f6be0bd273fa0f5d",
+    "url": "/github-profiles/static/js/main.97a1f4e6.chunk.js"
+  },
+  {
+    "revision": "22eb50dee70793f3fde7",
+    "url": "/github-profiles/static/js/runtime-main.84d61e65.js"
+  }
+]);
