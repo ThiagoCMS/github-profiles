53cdbd85d15d795bb64fa6b9cd3832bd01d8eb23
ThiagoCMS <thicesar25@gmail.com>
2020-09-18 12:30:37 -0300

    footer now using styled-components

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
 
-import './Footer.css';
+const FooterBase = styled.footer`
+  align-items: center;
+  background-color: #24292e;
+  display: flex;
+  height: 40px;
+  justify-content: center;
+`;
+
+const Text = styled.p`
+  color: #fff;
+`;
 
 function Footer() {
   return (
-    <footer className="footer">
-      <p>
+    <FooterBase>
+      <Text>
         Made with
         {' '}
         {'</>'}
         {' '}
         by Thiago
-      </p>
-    </footer>
+      </Text>
+    </FooterBase>
   );
 }
 
